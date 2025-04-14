0f493475a231ec4d25b8d576475521ae37a23e5c
AncoIsaac <166188398+AncoIsaac@users.noreply.github.com>
2025-04-14 14:32:35 -0600

    start proyect

---
+++ src/main/java/com/isaacAnco/chessv2/Chessv2Application.java
@@ -0,0 +1,13 @@
+package com.isaacAnco.chessv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chessv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chessv2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaacAnco/chessv2/Chessv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaacAnco.chessv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chessv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
