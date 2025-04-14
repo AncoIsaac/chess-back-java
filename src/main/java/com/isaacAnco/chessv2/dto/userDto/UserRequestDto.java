1593d370c0fe0dd393734c067810dca3b810d313
AncoIsaac <166188398+AncoIsaac@users.noreply.github.com>
2025-04-14 16:50:47 -0600

    add

--- src/main/java/com/isaacAnco/chessv2/dto/userDto/UserRequestDto.java
+++ src/main/java/com/isaacAnco/chessv2/dto/userDto/UserRequestDto.java
@@ -24,6 +24,6 @@ public class UserRequestDto {
     @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
-    @Schema(description = "User's name", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+    @Schema(description = "User's name")
     private String userName;
 }
